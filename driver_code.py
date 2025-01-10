4eceaf215dbdae8afe62017ffb8fa754170479f4
Vibhor Sakalley <vibhorsakalley@gmail.com>
2025-01-10 16:11:32 +0530

    updated code

--- driver_code.py
+++ driver_code.py
@@ -1,6 +1,7 @@
 import time
 import pymongo_conn as mongo_conn
 import scraping_data as nts
+from text_summarization import InsertSummariesIntoDatabase
 
 # Creating a driver class to handle the execuetion of the code
 # this class execuets the process of scraping and storing the data
@@ -11,7 +12,7 @@ class DriverClass:
         self.links = link
         self.filename = filename
     
-    # class for scraping the links
+    # scraping the links
     def scrapelinks(self, v2=None, v3=None, ref=None):
 
         try:
@@ -23,7 +24,8 @@ class DriverClass:
 
         except Exception as e:
                 return e
-
+    
+    # inserting the scraped data into MongoDB's collection
     def insert_to_db(self, feature_name=None, valst=None, start=None, end=None, val=None, val2=None, val3=None, val4=None, titleval=None, tagval=None, endcon=None):
 
         try:
@@ -40,44 +42,71 @@ class DriverClass:
 
 if __name__ == '__main__':
 
-    def main():
+    def main(scrape=False):
+        if scrape:
+            print("Starting scraping process...")
+            database_var = mongo_conn.MongoDBManager(db_name='Vibhor', collection_name='moneycontrol_news')
+            total_length = database_var.check_collection_length()
 
+            print('Total length: ', total_length)
+            start = total_length
+            end = start + 2
 
-        database_var = mongo_conn.MongoDBManager(db_name='Vibhor',collection_name='moneycontrol_news')
-        total_length = database_var.check_collection_length()
-        
-        print('total length: ',total_length)
-        start = total_length
-        end = (start + 2)
+            print(f'Start at {start} and end till {end}')
 
-        print(f'start at {start} and end till {end}')
+            scr_news = DriverClass(
+                link="https://www.moneycontrol.com/news/business/stocks/",
+                filename='20_sep_links.csv'
+            )
 
-        scr_news = DriverClass(link="https://www.moneycontrol.com/news/business/stocks/",
-                              filename='20_sep_links.csv')
+            scr_news.scrapelinks(v2='fleft', v3='a', ref='href')
 
-        scr_news.scrapelinks(v2='fleft', v3='a', ref='href')
+            coll = scr_news.insert_to_db(
+                feature_name='Links',
+                valst=['/news/business/stock', '/news/business'],
+                endcon=True,
+                start=start,
+                end=end,
+                titleval='page_left_wrapper',
+                tagval='h1',
+                val4='article_schedule',
+                val='clearfix',
+                val2='content_wrapper',
+                val3='p'
+            )
+            
+            print(coll)
+            print('Appending values...')
 
-        coll = scr_news.insert_to_db(feature_name='Links', valst=['/news/business/stock', '/news/business'], endcon=True, start=start, end=end, titleval='page_left_wrapper', tagval='h1',
-                               val4='article_schedule', val='clearfix', val2='content_wrapper', val3='p')
-        
-        print(coll)
-        print('appending values')
+            time.sleep(1)
+            database_var.insert_data_in_collection(data=coll)
+            database_var.close_conn()
+            time.sleep(1)
 
-        time.sleep(1)
-        database_var.insert_data_in_collection(data=coll)
-        database_var.close_conn()
-        time.sleep(1)
 
     def driver_func():
-
-        condition = input('do you want to scrape : ')
-
+        condition = input('Do you want to scrape (yes/no): ').strip().lower()
         if condition == 'yes':
-            main()
+            main(scrape=True)
         else:
-            print('exit')
+            print("Skipping scraping. Proceeding to summarization...")
+            main(scrape=False)
 
-        if condition == 'yes':
-            driver_func()
 
     driver_func()
+
+    db_var = mongo_conn.MongoDBManager(db_name='Vibhor', collection_name='text_summaries')
+    total_length = db_var.check_collection_length()
+    print(total_length)
+
+    summary = InsertSummariesIntoDatabase(database='Vibhor',collection='moneycontrol_news',featurename='text',slicer=total_length)
+    result_data = summary.get_summaries(chunk=1000,overlap=300,model_name='models/embedding-001',chain='stuff',query='give summary of the following')
+    print(len(result_data))
+    
+
+    print('inserting into database.....')
+
+    
+    db_var.insert_data_in_collection(data=result_data)
+    db_var.close_conn()
+
---
+++ text_summarization.py
@@ -0,0 +1,194 @@
+import os  # Importing the 'os' module for operating system related functionalities
+import pymongo_conn as mongo_conn
+from langchain.chains.question_answering import load_qa_chain  # Importing the function load_qa_chain from langchain library for question answering
+import google.generativeai as genai  # Importing the Google Generative AI module from the google package
+from langchain_google_genai import ChatGoogleGenerativeAI  # Importing ChatGoogleGenerativeAI class from langchain_google_genai module
+from langchain.text_splitter import RecursiveCharacterTextSplitter  # Importing RecursiveCharacterTextSplitter class from langchain module for text splitting
+from langchain_google_genai import GoogleGenerativeAIEmbeddings  # Importing GoogleGenerativeAIEmbeddings class from langchain_google_genai module
+from langchain_community.vectorstores import FAISS  # Importing FAISS class from langchain module for vector storage
+from langchain.prompts import PromptTemplate  # Importing PromptTemplate class from langchain module for prompts
+import warnings
+from pymongo_conn import MongoDBManager
+warnings.filterwarnings('ignore')
+# Setting the API key for Google Generative AI service by assigning it to the environment variable 'GOOGLE_API_KEY'
+api_key = os.environ['GOOGLE_API_KEY'] = 'xxxxxxxxxxx'
+
+# Configuring Google Generative AI module with the provided API key
+genai.configure(api_key=api_key)
+
+
+
+# class for reading the data
+# from the MongoDB Database
+class ReadData:
+    
+    # class constructor
+    def __init__(self,db=None, collectionname=None):
+
+        self.m = MongoDBManager(db_name=db,collection_name=collectionname)
+    
+    # reading the collection
+    # data as dataframe
+    def read_df(self):
+
+        try:
+            return self.m.read_collection_as_df()
+        except Exception as e:
+            return e
+    
+    # filtering the dataframe to get the summaries
+    def filter_data_as_str(self,news_text_featurename=None,slicer=None):
+
+        try:
+            df = self.read_df()
+            if slicer == 0:
+                return df[news_text_featurename].to_list()
+            else:
+                return df[news_text_featurename].to_list()[slicer:]
+        except Exception as e:
+            return e
+            
+
+# declaring a class 
+# for google-generative AI API
+class ChatGoogleGENAI:
+    def __init__(self):
+        
+        # Initializing the ChatGoogleGenerativeAI object with specified parameters
+        self.model = ChatGoogleGenerativeAI(
+            model="gemini-pro",  # Using the 'gemini-pro' model
+            temperature=0.3,  # Setting temperature for generation
+            google_api_key=api_key, # Passing the Google API key
+            top_p=1.0,
+            top_k=32,
+            candidate_count=1,
+            max_output_tokens=8192, 
+        )
+
+
+class EmbeddingModel:
+    def __init__(self, model_name):
+        # Initializing GoogleGenerativeAIEmbeddings object with the specified model name
+        self.embeddings = GoogleGenerativeAIEmbeddings(model=model_name)
+        
+class TextChunks:
+    @classmethod
+    def get_text_chunks(cls, separator=None, chunksize=None, overlap=None, text=None):
+        try:
+            # Splitting text into chunks based on specified parameters
+            text_splitter = RecursiveCharacterTextSplitter(separators=separator, chunk_size=chunksize, chunk_overlap=overlap)
+            return text_splitter.split_text(text)
+        except Exception as e:
+            return e
+        
+class Vectors:
+    @classmethod
+    def generate_vectors(cls, chunks, model):
+        try:
+            # Generating vectors from text chunks using specified model
+            embeddings = EmbeddingModel(model_name=model)
+            return FAISS.from_texts(chunks, embedding=embeddings.embeddings)
+        except Exception as e:
+            return e
+
+class DocumentSummarization(ChatGoogleGENAI):
+    def __init__(self,text):
+        super().__init__()  # Calling the constructor of the superclass (ChatGoogleGENAI)
+        # Reading text from the specified directory and assigning it to self.file
+        self.file = text
+    
+    def get_chunks(self, separator=None, chunksize=None, overlap=None):
+        try:
+            # Getting text chunks from the file using TextChunks class
+            return TextChunks().get_text_chunks(separator=separator, chunksize=chunksize, overlap=overlap, text=self.file)
+        except Exception as e:
+            return e
+        
+    def embeddings(self, separator=None, chunksize=None, overlap=None, model=None):
+        try:
+            # Generating vectors from text chunks using Vectors class
+            return Vectors().generate_vectors(chunks=self.get_chunks(separator, chunksize, overlap), model=model)
+        except Exception as e:
+            return e
+    
+    def summarisation_chains(self, chaintype):
+        try:
+            # Defining a prompt template for summarization
+            # the "context" variable provides the background or content from which the summary is derived
+            # the "question" variable prompts the user to focus on specific aspects or details within that context
+            prompt_template = """
+            You are given a text that contains daily stock news for indian stock market. 
+            Your job is generate a consice summary of the given text.
+            You have to make sure no information is missed.
+            Context:\n {context}?\n
+            Question: \n{question}\n
+        
+            Answer:
+            """
+            # Creating a PromptTemplate object with the defined template
+            prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+            # Loading the question-answering chain with the specified chain type and prompt
+            return load_qa_chain(self.model, chain_type=chaintype, prompt=prompt)
+        except Exception as e:
+            return e
+    
+    def main(self, separator=None, chunksize=None, overlap=None, model=None, type=None, user_question=None):
+        try:
+            # Retrieving the summarization chain
+            chain = self.summarisation_chains(chaintype=type)
+            # Generating embeddings for the text
+            db = self.embeddings(separator, chunksize, overlap, model)
+            # Performing similarity search and obtaining documents
+            docs = db.similarity_search(user_question)
+            # Generating response using the chain and user question
+            response = chain(
+                {"input_documents": docs, "question": user_question},
+                return_only_outputs=True
+            )
+            return response
+        except Exception as e:
+            return e
+        
+
+class InsertSummariesIntoDatabase:
+
+    def __init__(self,database=None,collection=None,featurename=None,slicer=None):
+
+        r = ReadData(db=database,collectionname=collection)
+        self.news_text = r.filter_data_as_str(news_text_featurename=featurename,slicer=slicer)
+
+    def create_list_of_dictionaries(self,data=None):
+
+        try:
+            return [{'summary': text} for text in data]
+        except Exception as e:
+            return e
+
+    def get_summaries(self,chunk=None,overlap=None,model_name=None,chain=None,query=None):
+
+        try:
+            # Process all data and store summaries in a single list
+            summaries = []
+            for data in self.news_text:
+                re = DocumentSummarization(text=data)
+                result = re.main(
+                    chunksize=chunk,
+                    overlap=overlap,
+                    model=model_name,
+                    type=chain,
+                    user_question=query
+                )
+                # Append the output text directly to the summaries list
+                summaries.append(result['output_text'])
+
+            # Print each summary line without a nested loop
+            
+            result_data = self.create_list_of_dictionaries(data=summaries)
+            return result_data
+        except Exception as e:
+            return e
+
+        
+if __name__ == '__main__':
+
+    pass
